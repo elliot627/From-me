f4321dff9d332d07ec8b8fd69dbac509e0908cb8
elliot627 <elliotwhitehead1@yahoo.com>
2015-01-30 09:52:15 -0600

    Created data set and video popup initialization! All appears to be in order, but second video is not loading properly.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,60 @@
 $(document).ready(function(){
 
-	//magnific-popup gallery initializer
-	$('.gallery-container').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		gallery: {
-			enabled: true,
-			preload: [1,2],
-			navigateByImgClick: true,
-			tPrev: 'Previous (left arrow key)',
-			tNext: 'Next (right arrow key)'
-		},
-		mainClass: 'mfp-with-zoom',
-		cursor: null
-	});
-
-	$('.open-popup').magnificPopup({
-		type: 'inline',
-		midClick: true,
-		alignTop: false,
-		showCloseBtn: true,
-		closeBtnInside: false
-	});
-
-
+	//prevents appending '#'s to URL for anchors which do not link to a new pagae
 	$('.not-a-link').click(function(e){
 		e.preventDefault();
 	});
+
+	//magnific-popup gallery initializer
+	// $('.gallery-container').magnificPopup({
+	// 	delegate: 'a',
+	// 	type: 'image',
+	// 	gallery: {
+	// 		enabled: true,
+	// 		preload: [1,2],
+	// 		navigateByImgClick: true,
+	// 		tPrev: 'Previous (left arrow key)',
+	// 		tNext: 'Next (right arrow key)'
+	// 	},
+	// 	mainClass: 'mfp-with-zoom',
+	// 	cursor: null
+	// });
+
+	var data = [
+		{
+			motionTitle: "Mr. Fox Has a Plan",
+			video_src: "videos/mrFox.mp4",
+			description: "Odd Future Banksy wolf Brooklyn, deep v fixie forage bitters trust fund Wes Anderson sriracha asymmetrical. Trust fund organic meditation ennui. Roof party quinoa next level distillery. Meh viral keytar, ennui you probably haven't heard of them fap fingerstache locavore slow-carb distillery organic 90's Vice Pitchfork Portland. Pickled artisan American Apparel Schlitz vinyl church-key, cray 90's biodiesel bicycle rights Pinterest Helvetica Kickstarter kitsch. Tattooed small batch Brooklyn, McSweeney's seitan church-key Pitchfork drinking vinegar crucifix. Pop-up Brooklyn Pitchfork, semiotics squid cardigan Wes Anderson stumptown readymade flexitarian."
+		},
+		{
+			motionTitle: "Just some family skiing",
+			video_src: "videos/skiing.mp4",
+			description: "Odd Future Banksy wolf Brooklyn, deep v fixie forage bitters trust fund Wes Anderson sriracha asymmetrical. Trust fund organic meditation ennui. Roof party quinoa next level distillery. Meh viral keytar, ennui you probably haven't heard of them fap fingerstache locavore slow-carb distillery organic 90's Vice Pitchfork Portland. Pickled artisan American Apparel Schlitz vinyl church-key, cray 90's biodiesel bicycle rights Pinterest Helvetica Kickstarter kitsch. Tattooed small batch Brooklyn, McSweeney's seitan church-key Pitchfork drinking vinegar crucifix. Pop-up Brooklyn Pitchfork, semiotics squid cardigan Wes Anderson stumptown readymade flexitarian."
+		}
+	];
+
+	$('.open-popup').magnificPopup({
+		items: data,
+		type: 'inline',
+		inline:{
+			markup:
+					'<div class="motion-popup"><div class="mfp-close"></div>'+
+						'<video>'+
+							'<source class="mfp-video" type="video/mp4">'+
+						'</video>'+
+						'<h2 class="mfp-motionTitle"></h2>'+
+						'<p class="mfp-description"></p>'+
+					'</div>'
+		},
+		gallery:{
+			enabled: true
+		},
+		midClick: true,
+		alignTop: false,
+		showCloseBtn: true,
+		closeBtnInside: true
+	});
+	
 
 	$(function() {
 		$('a[href*=#]:not([href=#])').click(function() {
