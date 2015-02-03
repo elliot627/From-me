33b4b1f167e5e1f815d20aedf6ce93564b85620f
elliot627 <elliotwhitehead1@yahoo.com>
2015-02-03 11:07:16 -0600

    Built photography gallery with popup lightbox

--- js/main.js
+++ js/main.js
@@ -12,62 +12,76 @@ $(document).ready(function(){
 	});
 
 	//magnific-popup gallery initializer
-	// $('.gallery-container').magnificPopup({
-	// 	delegate: 'a',
-	// 	type: 'image',
-	// 	gallery: {
-	// 		enabled: true,
-	// 		preload: [1,2],
-	// 		navigateByImgClick: true,
-	// 		tPrev: 'Previous (left arrow key)',
-	// 		tNext: 'Next (right arrow key)'
-	// 	},
-	// 	mainClass: 'mfp-with-zoom',
-	// 	cursor: null
-	// });
-
-	var data = [
-		// {
-		// 	motionTitle: "Mr. Fox Has a Plan",
-		// 	source_src: "videos/mrfox.mp4",
-		// 	video_id: "fox",
-		// 	description: "Odd Future Banksy wolf Brooklyn, deep v fixie forage bitters trust fund Wes Anderson sriracha asymmetrical. Trust fund organic meditation ennui. Roof party quinoa next level distillery. Meh viral keytar, ennui you probably haven't heard of them fap fingerstache locavore slow-carb distillery organic 90's Vice Pitchfork Portland. Pickled artisan American Apparel Schlitz vinyl church-key, cray 90's biodiesel bicycle rights Pinterest Helvetica Kickstarter kitsch. Tattooed small batch Brooklyn, McSweeney's seitan church-key Pitchfork drinking vinegar crucifix. Pop-up Brooklyn Pitchfork, semiotics squid cardigan Wes Anderson stumptown readymade flexitarian."
-		// },
-		{
-			motionTitle: "Family skiing",
-			source_src: "videos/skiing.mp4",
-			video_id: "skiing",
-			description: "Some other dynamic content Odd Future Banksy wolf Brooklyn, deep v fixie forage bitters trust fund Wes Anderson sriracha asymmetrical. Trust fund organic meditation ennui. Roof party quinoa next level distillery. Meh viral keytar, ennui you probably haven't heard of them fap fingerstache locavore slow-carb distillery organic 90's Vice Pitchfork Portland. Pickled artisan American Apparel Schlitz vinyl church-key, cray 90's biodiesel bicycle rights Pinterest Helvetica Kickstarter kitsch. Tattooed small batch Brooklyn, McSweeney's seitan church-key Pitchfork drinking vinegar crucifix. Pop-up Brooklyn Pitchfork, semiotics squid cardigan Wes Anderson stumptown readymade flexitarian."
+	$('.photography-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery: {
+			enabled: true,
+			preload: [1,2],
+			navigateByImgClick: true,
+			tPrev: 'Previous (left arrow key)',
+			tNext: 'Next (right arrow key)'
 		},
-		{
-			motionTitle: "Warpaint - Lyric Video",
-			source_src: "videos/warpaint.mov",
-			video_id: "warpaint",
-			description: "You want to bottle me up, lock away my heart and soul. You came searching for a rock, that you could build your home upon. But I'm a rolling stone, I'm a rolling stone. And what you need the most, what you, what you need the most is what I have the least"
+		image: {
+			markup: '<div class="mfp-figure">'+
+						'<div class="mfp-close"></div>'+
+						'<div class="mfp-img"></div>'+
+						'<div class="mfp-bottom-bar">'+
+							'<div class="mfp-title"></div>'+
+							'<div class="mfp-counter"></div>'+
+						'</div>'+
+					'</div>',
+			cursor: 'mfp-zoom-out',
+			verticalFit: true,
+			tError: '<a href="%url%">The image</a> could not be loaded.'
 		}
-	];
-
-	$('.open-motion-popup').magnificPopup({
-		items: data,
-		type: 'inline',
-		inline:{
-			markup:
-					'<div class="motion-popup"><div class="mfp-close"></div>'+
-						'<video controls>'+
-							'<source class="mfp-source" type="video/mp4">'+
-						'</video>'+
-						'<h2 class="mfp-motionTitle"></h2>'+
-						'<p class="mfp-description"></p>'+
-					'</div>'
-		},
-		gallery:{
-			enabled: true
-		},
-		midClick: true,
-		alignTop: false,
-		showCloseBtn: true,
-		closeBtnInside: true
 	});
+
+
+	//Magnific popup initialzer for motion videos
+	// var data = [
+	// 	{
+	// 		motionTitle: "Mr. Fox Has a Plan",
+	// 		source_src: "videos/mrfox.mp4",
+	// 		video_id: "fox",
+	// 		description: "Odd Future Banksy wolf Brooklyn, deep v fixie forage bitters trust fund Wes Anderson sriracha asymmetrical. Trust fund organic meditation ennui. Roof party quinoa next level distillery. Meh viral keytar, ennui you probably haven't heard of them fap fingerstache locavore slow-carb distillery organic 90's Vice Pitchfork Portland. Pickled artisan American Apparel Schlitz vinyl church-key, cray 90's biodiesel bicycle rights Pinterest Helvetica Kickstarter kitsch. Tattooed small batch Brooklyn, McSweeney's seitan church-key Pitchfork drinking vinegar crucifix. Pop-up Brooklyn Pitchfork, semiotics squid cardigan Wes Anderson stumptown readymade flexitarian."
+	// 	},
+	// 	{
+	// 		motionTitle: "Family skiing",
+	// 		source_src: "videos/skiing.mp4",
+	// 		video_id: "skiing",
+	// 		description: "Some other dynamic content Odd Future Banksy wolf Brooklyn, deep v fixie forage bitters trust fund Wes Anderson sriracha asymmetrical. Trust fund organic meditation ennui. Roof party quinoa next level distillery. Meh viral keytar, ennui you probably haven't heard of them fap fingerstache locavore slow-carb distillery organic 90's Vice Pitchfork Portland. Pickled artisan American Apparel Schlitz vinyl church-key, cray 90's biodiesel bicycle rights Pinterest Helvetica Kickstarter kitsch. Tattooed small batch Brooklyn, McSweeney's seitan church-key Pitchfork drinking vinegar crucifix. Pop-up Brooklyn Pitchfork, semiotics squid cardigan Wes Anderson stumptown readymade flexitarian."
+	// 	},
+	// 	{
+	// 		motionTitle: "Warpaint - Lyric Video",
+	// 		source_src: "videos/warpaint.mov",
+	// 		video_id: "warpaint",
+	// 		description: "You want to bottle me up, lock away my heart and soul. You came searching for a rock, that you could build your home upon. But I'm a rolling stone, I'm a rolling stone. And what you need the most, what you, what you need the most is what I have the least"
+	// 	}
+	// ];
+
+	// $('.open-motion-popup').magnificPopup({
+	// 	items: data,
+	// 	type: 'inline',
+	// 	inline:{
+	// 		markup:
+	// 				'<div class="motion-popup"><div class="mfp-close"></div>'+
+	// 					'<video controls>'+
+	// 						'<source class="mfp-source" type="video/mp4">'+
+	// 					'</video>'+
+	// 					'<h2 class="mfp-motionTitle"></h2>'+
+	// 					'<p class="mfp-description"></p>'+
+	// 				'</div>'
+	// 	},
+	// 	gallery:{
+	// 		enabled: true,
+	// 		preload: [0,2]
+	// 	},
+	// 	midClick: true,
+	// 	alignTop: false,
+	// 	showCloseBtn: true,
+	// 	closeBtnInside: true
+	// });
 	
 
 	$(function() {
